35ae4345363f503b05681ee3473e680dc11bc9eb
Tuncay Demir <tuncaydemir682@gmail.com>
2022-04-20 05:52:49 +0300

    Create coefficientsum.java

---
+++ coefficientsum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class coefficientsum{
+    public static void main(String[] args) {
+
+            int a, total = 0;
+
+            Scanner input = new Scanner(System.in);
+
+
+            do {
+
+                System.out.print("Bir sayı gir : ");
+
+                a = input.nextInt();
+
+                if(a % 4 == 0){
+
+                    total += a;
+                }
+            } while(a % 2 == 0);
+
+            System.out.print("Total : " + total);
+    }
+}
